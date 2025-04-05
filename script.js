c1a616e86f2f8935f825df499fd315a0fc4025eb
shravan kumar <117439152+borrolla@users.noreply.github.com>
2025-04-05 17:10:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,48 +1,47 @@
 //your JS code here. If required.
-const usernameInput = document.getElementById('username');
-const passwordInput = document.getElementById('password');
-const rememberCheckbox = document.getElementById('checkbox');
-const submitBtn = document.getElementById('submit');
-const existingBtn = document.getElementById('existing');
+// const usernameInput = document.getElementById('username');
+// const passwordInput = document.getElementById('password');
+// const rememberCheckbox = document.getElementById('checkbox');
+// const submitBtn = document.getElementById('submit');
+// const existingBtn = document.getElementById('existing');
 
 // Check localStorage on load
 window.onload  = function() {
-	const savedUsername = localStorage.getItem('username');
-	const savedPassword = ;pcalStorage.getItem('password');
-
-	if (savedUsername && savedPassword) {
-        existingBtn.style.display = 'inline-block';
-      } else {
-        existingBtn.style.display = 'none';
+	const savedUsername = localStorage.getItem("username");
+	const savedPassword = localStorage.getItem("password");
+      
+      if (savedUsername && savedPassword) {
+        document.getElementById("existing").style.display = "inline-block";
       }
 };
 // Submit behavior
-document.getElementById('loginForm').addEventListener('submit',function (e) {
+document.getElementById("loginForm").addEventListener("submit",function (e) {
 	e.preventDefault();
-	const username = usernameInput.value;
-      const password = passwordInput.value;
+	const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+	 const remember = document.getElementById("checkbox").checked;
 
-	alert(`Logged in as ${username}`);
+	 alert("Logged in as " + username);
 
 	if(rememberCheckbox.checked){
-		localStorage.setItem('username', username);
-		localStorage.setItem('password', password);
-		 existingBtn.style.display = 'inline-block';
+		localStorage.setItem("username", username);
+		localStorage.setItem("password", password);
+		 document.getElementById("existing").style.display = "inline-block";
 	}
 	else
 	{
 		localStorage.removeItem('username');
         localStorage.removeItem('password');
-        existingBtn.style.display = 'none';
+       document.getElementById("existing").style.display = "none";
 	}
 	
 });
 // Login as existing user
-existingBtn.addEventListener('click', function () => {
-	const savedUsername = localStorage.getItem('username');
-	if(savedUsername){
-		alert(`Logged in as ${savedUsername} `);
-	}
+document.getElementById("existing").addEventListener("click", function () {
+      const savedUsername = localStorage.getItem("username");
+      if (savedUsername) {
+        alert("Logged in as " + savedUsername);
+      }
 });
 
 
